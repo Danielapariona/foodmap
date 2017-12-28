d5b93883a10b5c7183916fb1b699c89193b2fbc0
Danielapariona <daniela.parionac@gmail.com>
2017-12-28 00:10:57 -0500

    Show cards of restaurants

--- js/app.js
+++ js/app.js
@@ -3,3 +3,37 @@ $(function () {
     $('.loader-wrap').fadeOut(500);
   }, 1000);
 });
+
+/* */
+
+var $contentCard = $('#content-card');
+var $cardRestaurant = $('.card-restaurant');
+
+/* Data */
+
+/*$restaurant[0]['name'] */
+var $place = data["CANETE"]["Imperial"];
+var $restaurant = $place["restaurant"];
+
+
+for (var i = 0; i < $restaurant.length; i++) {
+  // crea los contenedores
+  var $card = $("<div class='card-restaurant'><a href='#' class='link center'><div class='data'></div></a></div>");
+  $contentCard.append($card);
+  var $data = $('.data');
+  var $link = $('.link');
+  // Muestra dinamicamente los datos
+  var $keys = ['name', 'address', 'time','photo'];
+  for (var j = 0; j < $keys.length; j++) {
+    var $a = $restaurant[i][$keys[j]];
+    if ($keys[j] == 'photo') {
+      var $background = $("<div class='photo-restaurant'>");
+      $background.css('background-image','url' + '(' + $restaurant[i]['photo']  + ')');
+      $link.eq(i).attr("href", $restaurant[i]['link']);
+      $link.eq(i).append($background);
+      continue;
+    } else {
+      $data.eq(i).append('<p>' + $a + '</p>');
+    }
+  }
+}
--- js/data.js
+++ js/data.js
@@ -2,51 +2,153 @@ var data = {
   'CANETE': {
     'Imperial': {
       'restaurant': [{
-        'name': 'Restaurant Chicharronería Charapito',
-        'address': 'Jirón Ayacucho 198',
-        'type': [
-          'Criollo', 'Chicharronería'
-        ],
-        'foods': {
-          'entry': [{
-              'name': 'Papa a la Huacaína',
-              'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-              'price': 8
-            },
-            {
-              'name': 'Ensalada Mixta',
-              'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-              'price': 10
-            },
-            {
-              'name': 'Ensalada Popeye',
-              'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-              'price': 12
-            }
+          'name': 'Restaurant Chicharronería Charapito',
+          'address': 'Jirón Ayacucho 198',
+          'time':'17:00',
+          'photo':'http://dummyimage.com/162x148.png/ff4444/ffffff',
+          'link':'Charapito.html',
+          'type': [
+            'Criollo', 'Chicharronería'
           ],
-          'main course': [{
-              'name': 'Chicharrones de chancho',
-              'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-              'price': 15
-            },
-            {
-              'name': 'Lomo Saltado',
-              'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-              'price': 21
-            },
-            {
-              'name': 'Seco de pato',
-              'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-              'price': 35
-            },
-            {
-              'name': 'Arroz con pollo',
-              'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-              'price': 12
-            }
-          ]
+          'foods': {
+            'entry': [{
+                'name': 'Papa a la Huacaína',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 8
+              },
+              {
+                'name': 'Ensalada Mixta',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 10
+              },
+              {
+                'name': 'Ensalada Popeye',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 12
+              }
+            ],
+            'main course': [{
+                'name': 'Chicharrones de chancho',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 15
+              },
+              {
+                'name': 'Lomo Saltado',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 21
+              },
+              {
+                'name': 'Seco de pato',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 35
+              },
+              {
+                'name': 'Arroz con pollo',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 12
+              }
+            ]
+          }
+        },
+        {
+          'name': 'Restaurant Chicharronería Charapito II',
+          'address': 'Jirón Ayacucho 198',
+          'time': '23:30',
+          'photo':'https://dummyimage.com/162x148.png/frdesw',
+          'link':'../adela.html',
+          'type': [
+            'Criollo', 'Chicharronería'
+          ],
+          'foods': {
+            'entry': [{
+                'name': 'Papa a la Huacaína',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 8
+              },
+              {
+                'name': 'Ensalada Mixta',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 10
+              },
+              {
+                'name': 'Ensalada Popeye',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 12
+              }
+            ],
+            'main course': [{
+                'name': 'Chicharrones de chancho',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 15
+              },
+              {
+                'name': 'Lomo Saltado',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 21
+              },
+              {
+                'name': 'Seco de pato',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 35
+              },
+              {
+                'name': 'Arroz con pollo',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 12
+              }
+            ]
+          }
+        },
+        {
+          'name': 'Restaurant Chicharronería Charapito III',
+          'address': 'Jirón Ayacucho 198',
+          'time': '23:30',
+          'photo':'https://dummyimage.com/162x148.png/cdcdsd',
+          'link':'../adela.html',
+          'type': [
+            'Criollo', 'Chicharronería'
+          ],
+          'foods': {
+            'entry': [{
+                'name': 'Papa a la Huacaína',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 8
+              },
+              {
+                'name': 'Ensalada Mixta',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 10
+              },
+              {
+                'name': 'Ensalada Popeye',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 12
+              }
+            ],
+            'main course': [{
+                'name': 'Chicharrones de chancho',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 15
+              },
+              {
+                'name': 'Lomo Saltado',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 21
+              },
+              {
+                'name': 'Seco de pato',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 35
+              },
+              {
+                'name': 'Arroz con pollo',
+                'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+                'price': 12
+              }
+            ]
+          }
         }
-      }]
+      ]
     }
   }
 }
